f8c8f90a197f56aeae12557ee3abe8107c1aa0f7
Rumi Novruz <127120972+RumiNovruz@users.noreply.github.com>
2024-03-31 23:30:24 +0400

    last_1

---
+++ app/src/main/java/com/example/thenewsapp/ui/SplashActivity.kt
@@ -0,0 +1,22 @@
+package com.example.thenewsapp.ui
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.example.thenewsapp.R
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, NewsActivity::class.java)
+            startActivity(intent)
+            finish()
+        },3000)
+    }
+}
